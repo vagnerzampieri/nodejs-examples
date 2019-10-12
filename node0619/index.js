301be2d3b06c050ed639adc84a2e133490be9b89
Vagner Zampieri <vgzampieri@gmail.com>
2019-10-12 17:22:57 -0300

    Use exports without ES6

--- node0619/index.js
+++ node0619/index.js
@@ -1,11 +1,4 @@
-function X() {
-  console.log(arguments);
-}
+console.log("In index.js");
 
-X(1, 3, 5);
-// =>[Arguments] { '0': 1, '1': 3, '2': 5 }
-
-// WRAPPING, IIFE
-// require('module').wrapper
-// 'function(exports, require, module, _flename, _dirname)'
-// console.log(arguments);
+exports.answer = 42;
+module.exports.answer2 = 37;
