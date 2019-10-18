df9efe31a1aa9917334458dd82aa188be3b2e6af
Vagner Zampieri <vgzampieri@gmail.com>
2019-10-18 20:08:19 -0300

    Use HTTP to create a server

--- example-4/server.js
+++ example-4/server.js
@@ -1,3 +1,11 @@
 import http from "http";
 
-console.log(http);
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request!!!", req.url);
+  res.write("Hello HTTP");
+  res.end();
+});
+
+server.listen(3000);
