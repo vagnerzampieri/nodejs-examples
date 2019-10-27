bf2c0f1c59ddc761bdb68430ff2a527ff09256d0
Vagner Zampieri <vgzampieri@gmail.com>
2019-10-27 11:58:03 -0300

    Remove unncessary fields in the query

--- example-9/routes/users.js
+++ example-9/routes/users.js
@@ -7,10 +7,10 @@ const router = express.Router();
 // /api/users
 router.get("/", verifyToken, async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find().select("_id name email date");
     res.json(users);
   } catch (error) {
-      res.json({ message: error})
+    res.json({ message: error });
   }
 });
 
