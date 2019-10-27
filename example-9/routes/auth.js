c82ef19460d2132328284d271e56f1a9de821105
Vagner Zampieri <vgzampieri@gmail.com>
2019-10-27 09:53:13 -0300

    Use Joi to validate  data

--- example-9/routes/auth.js
+++ example-9/routes/auth.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { User } from "../models/User";
+import { registerValidation } from "../validations/register";
 
 const router = express.Router();
 
 // /api/user/register
 router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
---
+++ example-9/validations/register.js
@@ -0,0 +1,19 @@
+import Joi from "@hapi/joi";
+
+const registerValidation = data => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(6)
+      .required(),
+    email: Joi.string()
+      .min(6)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(6)
+      .required()
+  });
+  return schema.validate(data);
+};
+
+export { registerValidation };
