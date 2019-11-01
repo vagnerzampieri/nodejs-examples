421917ec9cd964935af0e82f6d3c28b6a01f62f0
Vagner Zampieri <vgzampieri@gmail.com>
2019-11-01 19:30:00 -0300

    Make some test

--- example-10/functions.test.js
+++ example-10/functions.test.js
@@ -3,3 +3,7 @@ import { functions } from "./functions";
 test("Adds 2 + 2 equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
