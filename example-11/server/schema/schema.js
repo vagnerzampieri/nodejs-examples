bcc82e0785cf4fd6979608a3d2b83fa79fb9acdb
Vagner Zampieri <vgzampieri@gmail.com>
2019-11-10 19:42:16 -0300

    Add GraphQLNonNull for not accept null values

--- example-11/server/schema/schema.js
+++ example-11/server/schema/schema.js
@@ -4,7 +4,8 @@ import {
   GraphQLID,
   GraphQLString,
   GraphQLInt,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } from "graphql";
 import { Book } from "../models/Book";
 import { Author } from "../models/Author";
@@ -77,8 +78,8 @@ const Mutation = new GraphQLObjectType({
     addAuthor: {
       type: AuthorType,
       args: {
-        name: { type: GraphQLString },
-        age: { type: GraphQLInt }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) }
       },
       resolve(parent, args) {
         let author = new Author({
@@ -91,9 +92,9 @@ const Mutation = new GraphQLObjectType({
     addBook: {
       type: BookType,
       args: {
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString },
-        authorId: { type: GraphQLID }
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        genre: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(GraphQLID) }
       },
       resolve(parent, args) {
         let book = new Book({
