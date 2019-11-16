cec23e1d2c3547ce02e942889e61332417e7198b
Vagner Zampieri <vgzampieri@gmail.com>
2019-11-16 16:49:42 -0300

    Show Book List

--- example-11/client/src/App.js
+++ example-11/client/src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql"
+});
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Ninja's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
